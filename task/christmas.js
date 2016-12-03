e9f08c4b00985d3d282dbff5399765640cad7c7c
Administrator <15196004361@163.com>
2016-12-03 20:57:57 +0800

    圣诞节H5第一章节：运动的圣诞老人

--- task/christmas.js
+++ task/christmas.js
@@ -1,8 +1,48 @@
+
 /**
- * 慕课网特制
- * 圣诞主题效果
- * @type {Object}
+ * 背景音乐
  */
+function HTML5Audio(url, loop) {
+    var audio = new Audio(url);
+    audio.autoplay = true;
+    audio.loop = loop || false; //是否循环
+    audio.play();
+    return {
+        end: function(callback) {
+            audio.addEventListener('ended', function() {
+                callback()
+            }, false);
+        },
+        paused: function() {
+            audio.addEventListener('paused', function(){
+                audio.pause();
+            }, false);
+        }
+    }
+}
+/*HTML5Audio.prototype.play = function (loop) {
+    this.audio.autoplay = true;
+    this.audio.loop = loop || false;
+    if (this.audio.paused) {
+        this.audio.play();
+        console.log(this.audio);
+    } else {
+        this.audio.pause();
+
+    }
+}
+HTML5Audio.prototype.paused = function() {
+
+    if (this.audio.paused) {
+        this.audio.play();
+        console.log("1: "+this.audio);
+    } else {
+        this.audio.pause();
+        console.log(this.audio.paused);
+
+    }
+}*/
+
 
 /**
  * 切换页面
@@ -35,9 +75,10 @@ var Christmas = function() {
     var $pageB = $(".page-b");
     var $pageC = $(".page-c");
 
-    new pageA(changePagePromise($pageA, 'effect-out').then(function(){
-        new pageB(changePagePromise($pageC, 'effect-in'))
-    }))
+    //new pageA(changePagePromise($pageA, 'effect-out').then(function(){
+    //    new pageB(changePagePromise($pageC, 'effect-in'))
+    //}))
+    new pageA($pageA);
 
 };
 
@@ -46,4 +87,14 @@ var Christmas = function() {
 $(function() {
     //圣诞主题效果，开始
     $("#choose").on('click', Christmas);
+    $(".audioPlay").click(function () {
+        var h5Audio = HTML5Audio("http://www.sunnylinner.com/Games/Music/Media/407.mp3", false);
+        h5Audio.paused();
+    });
+
+    $(".endPlay").click(function () {
+        Christmas();
+    });
 })
+
+
--- task/pageA.js
+++ task/pageA.js
@@ -1,5 +1,64 @@
-function pageA(cb) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {resolve(cb)}, 2000);
-    })
+function pageA(ele) {
+    this.$root = ele;
+    this.boy = $('.chs-boy');
+    console.log(this.boy);
+    this.run();
 }
+
+pageA.prototype.next = function (options) {
+    var _this = this;
+    var promise = new Promise((resolve, reject) => {
+        _this.boy.css({
+            "top": options.style.top,
+            "right": options.style.right,
+            "transform": options.style.transform || options.style.rotateY,
+            "transition-property": "all",
+            "transition-duration": options.time,
+            "transition-timing-function": "linear"
+        });/*.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", () => {
+                console.log("animationend")
+            });*/
+        var timeout = setTimeout(resolve, parseInt(options.time,10)*1000);
+    });
+    return promise;
+};
+
+pageA.prototype.stopAnimation = function() {
+    this.boy.removeClass("chs-boy-deer");
+};
+
+pageA.prototype.run = function() {
+    var _this = this;
+     _this.next({
+        time:"8s",
+        style: {
+            top: "4rem",
+            right: '16rem',
+            transform: 'scale(1.2)'
+        }
+    })
+         .then(function () {
+             console.log("resolved2");
+             return _this.next({
+                 "time":"1s",
+                 "style": {
+                     "rotateY" : "rotateY(-180deg)"
+                 }
+             })
+         })
+        .then(function() {
+            console.log("resolved3");
+            return _this.next({
+                time:"5s",
+                style: {
+                    top: "8rem",
+                    right: '1.2rem',
+                    "rotateY" : "rotateY(-180deg)"
+                }
+            })
+        })
+         .then(function() {
+             _this.stopAnimation();
+         })
+
+};
